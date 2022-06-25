b62a36c20d5eed793f80c0a7846bb45db95766cd
Liza Zhukova <Elishkaaaa@yandex.ru>
2022-06-25 22:30:25 +0300

    first

---
+++ index.js
@@ -0,0 +1,7 @@
+let popup = document.querySelector('.popup')
+let editButton = document.querySelector('.profile__info_edit-button')
+
+
+editButton.addEventListener('click', function (){
+    popup.classList.add('.popup__opened')
+});
