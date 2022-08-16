e85c27493407b52829d9ae127777f656c927087e
Liza Zhukova <Elishkaaaa@yandex.ru>
2022-08-16 17:02:29 +0300

    add card

--- scripts/index.js
+++ scripts/index.js
@@ -4,10 +4,12 @@ let nameInput = document.getElementById('name');
 let jobInput = document.getElementById('opinion');
 let nameOfProfile = document.querySelector('.profile__info-name');
 let opinionOfProfile = document.querySelector('.profile__info-opinion');
+let addButton = document.querySelector('.profile__add-button');
 let closeButton = document.querySelector('.popup__close');
 let formElement = document.querySelector('.popup__container');
+let bigPhoto = document.querySelector('.popup_photo');
 
-
+//редактировать профиль
 function openPopap(){
     popup.classList.add('popup_opened');
     nameInput.value = nameOfProfile.textContent;
@@ -21,17 +23,57 @@ function closePopap(){
 
 
 
-
 function formSubmitHandler (evt) {
     evt.preventDefault();
-    
     nameOfProfile.textContent = nameInput.value
     opinionOfProfile.textContent = jobInput.value
     closePopap()
-}
+};
+
 
 formElement.addEventListener('submit', formSubmitHandler);
 editButton.addEventListener('click', openPopap);
-closeButton.addEventListener('click',closePopap);
+closeButton.addEventListener('click', closePopap);
 
 
+
+// форма добавления карточки
+let formCard = document.querySelector('.popup__container_card');
+let closeButtonCardForm = document.querySelector('.popup__close_card');
+let cardPopap = document.querySelector('.popup_add-card');
+let titleInput = document.querySelector('#title');
+let linkInput = document.querySelector('#link');
+let element = document.querySelector('.element');
+
+
+function openformCard(){
+  cardPopap.classList.add('popup_opened');
+};
+
+function closeCard(){
+  cardPopap.classList.remove('popup_opened');
+};
+
+
+function addCard(evt){
+  evt.preventDefault();
+  let template = document.querySelector('#template');
+  let item = template.content.cloneNode(true);
+  item.querySelector('.element__card-item-text').textContent = titleInput.value;
+  item.querySelector('.element__card-img').setAttribute('src', linkInput.value);
+  element.append(item);
+  closeCard();
+  titleInput.value = '';
+  linkInput.value ='';
+};
+
+
+addButton.addEventListener('click', openformCard);
+closeButtonCardForm .addEventListener('click', closeCard);
+formCard.addEventListener('submit', addCard);
+
+
+
+//добавить или убрать лайк
+
+   
